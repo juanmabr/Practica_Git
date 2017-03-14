5786b49a2973f6498c3f39cc0293095424979bdd
Juan Manuel Burgos <juanmabr@hotmail.es>
2017-03-14 13:23:27 +0100

    añadimos archivos

---
+++ Lib.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author ???
+ */
+
+public class Lib {
+
+    public void metodoAlumno1(){
+        System.out.println("metodo 1");
+    }
+
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+    }
+
+    public void metodoComunitario(){
+        System.out.println("Aqui escribimos todos");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author ???
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Clase principal del programa
+        System.out.println("Estoy en la clase Main");
+        Lib git = new Lib();
+
+        // Llamamos a los metodos de la clase auxiliar
+        git.metodoAlumno1();
+        git.metodoAlumno2();
+        git.metodoComunitario();
+    }
+}
