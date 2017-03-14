43a4ba301c62305d3f676d9bb52395769782b318
Juan Manuel Burgos <juanmabr@hotmail.es>
2017-03-14 19:53:31 +0100

    cambios en metodo3

--- Lib.java
+++ Lib.java
@@ -10,10 +10,10 @@ public class Lib {
     }
 
     public void metodoAlumno2(){
-        System.out.println("metodo 2");
+        metodo3();
     }
 
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
+        System.out.println("cambio_en_metodo3");
     }
 }
