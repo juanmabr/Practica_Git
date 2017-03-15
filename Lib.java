0cd655256d41d11c72b8708e7045d2f1aa4301a7
cristobal nuñez cobos <c.n.cobos@gmail.com>
2017-03-15 09:57:54 +0100

    modificaciones alumno 1

--- Lib.java
+++ Lib.java
@@ -1,26 +1,18 @@
-/**
- *
- * Juan Manuel Burgos Romero
- */
-
 public class Lib {
 
     public void metodoAlumno1(){
-        metodoAlumno2();
+        System.out.println("metodo 1");
     }
 
     public void metodoAlumno2(){
-        metodo3();
+        System.out.println("metodo 2");
+	System.out.println("modificado por alumno 2"); // alumno 2
     }
 
     public void metodoComunitario(){
-
-        System.out.println("cambio_en_master");
-
-        System.out.println("cambio_en_metodo3");
-
+	// Comentario incluido para pruebas
+	System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia");
     }
-    public void metodo3 (){
-    	System .out . println (" metodo  3");
-}
 }
