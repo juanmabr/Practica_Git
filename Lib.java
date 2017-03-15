a3370aef0c20469abff23fcf8ee7d9f6fe3cde7b
Juan Manuel Burgos <juanmabr@hotmail.es>
2017-03-15 09:47:53 +0100

    modificaciones alumno 1

--- Lib.java
+++ Lib.java
@@ -1,26 +1,18 @@
-/**
- *
- * Juan Manuel Burgos Romero
- */
-
 public class Lib {
 
     public void metodoAlumno1(){
-        metodoAlumno2();
+        System.out.println("metodo 1");
+        System.out.println("modificado por alumno 1"); // alumno 1
     }
 
     public void metodoAlumno2(){
-        metodo3();
+        System.out.println("metodo 2");
     }
 
     public void metodoComunitario(){
-
-        System.out.println("cambio_en_master");
-
-        System.out.println("cambio_en_metodo3");
-
-    }
-    public void metodo3 (){
-    	System .out . println (" metodo  3");
+	// Comentario incluido para pruebas
+	System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 1
+	System.out.println("Alumno 1 inserta esta sentencia");
 }
 }
